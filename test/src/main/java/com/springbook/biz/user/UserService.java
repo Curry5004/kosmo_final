351b8edfb03c3fb8ee46ba0383d9644826246dde
Curry5004 <yunjin5004@naver.com>
2022-02-28 16:01:23 +0900

    [홍지환] 개발 환경 업로드

---
+++ test/src/main/java/com/springbook/biz/user/UserService.java
@@ -0,0 +1,8 @@
+package com.springbook.biz.user;
+
+import java.util.List;
+
+public interface UserService {
+
+	List<UserVO> getUserList(UserVO vo);
+}
---
+++ test/src/main/java/com/springbook/biz/user/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.springbook.biz.user.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springbook.biz.user.UserService;
+import com.springbook.biz.user.UserVO;
+
+@Service("userService")
+public class UserServiceImpl implements UserService {
+	@Autowired
+	private UserDAOMybatis userDAO;
+
+	public List<UserVO> getUserList(UserVO vo) {
+		return userDAO.getUserList(vo);
+	}
+}

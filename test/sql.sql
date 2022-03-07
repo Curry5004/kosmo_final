f9861660bbaf6ba5aedf79d5b64f38a2bda6f025
ChanSol <mutongzang@gmail.com>
2022-03-08 00:33:17 +0900

    [정찬솔] sql 업데이트

--- test/sql.sql
+++ test/sql.sql
@@ -14,7 +14,7 @@ CREATE TABLE `USER` (
 	`BIRTHDAY`	VARCHAR(255)	NOT NULL,
 	`GENDER`	VARCHAR(255) NOT NULL,
 	`LOCATION`	VARCHAR(255)	NOT NULL,
-	`PHONE_NUM`	VARCHAR(255) NOT	NULL,
+	`PHONE_NUM`	INT NOT	NULL,
 	`JOIN_DATE`	DATETIME	NOT NULL,
 	`ADMIN`	BOOLEAN	default false,
 	`PROFILE_IMAGE`	VARCHAR(255) 	NULL,
@@ -55,6 +55,7 @@ CREATE TABLE `MEMBER_LIST` (
 	`USER_ID`	VARCHAR(255)	NOT NULL,
 	`PARTY_ID`	INT	NOT NULL,
     `PARTY_REQUEST` BOOLEAN DEFAULT FALSE,
+    `REG_DATE` DATETIME default NOW(),
     PRIMARY KEY(`USER_ID`,`PARTY_ID`)
 );
 
@@ -90,9 +91,9 @@ CREATE TABLE `BOARD_COMMENT` (
 CREATE TABLE `ALBUM` (
 	`ALB_ID`	INT	 auto_increment NOT NULL,
 	`PARTY_ID`	INT	NOT NULL,
-	`ALB_WRITER`	VARCHAR(255) 	NOT NULL,
+	`USER_ID`	VARCHAR(255) 	NOT NULL,
 	`ALB_IMG_PATH`	VARCHAR(255) 	NOT NULL,
-	`ALB_REG_DATE`	DATETIME	DEFAULT NOW(),
+	`ALB_REG_DATE`	DATETIME DEFAULT NOW(),
 	`ALB_AVAILABLE`	BOOLEAN	default true,
     PRIMARY KEY(`ALB_ID`,`PARTY_ID`)
 );
@@ -108,7 +109,7 @@ CREATE TABLE `SCHEDULE` (
 	`SCH_NOTE`	VARCHAR(255) 	NOT NULL,
 	`SCH_MEMBER_COUNT`	INT	DEFAULT 1,
 	`SCH_DATE`	DATETIME	NOT NULL,
-	`SCH_AVLILABLE`	BOOLEAN	default true,
+	`SCH_AVAILABLE`	BOOLEAN	default true,
     PRIMARY KEY(`SCH_ID`,`PARTY_ID`)
 );
 
@@ -182,13 +183,13 @@ CREATE TABLE `LIKE_LIST` (
 CREATE TABLE `ALBUM_COMMENT` (
 	`ALB_COMMENT_ID`	INT auto_increment	NOT NULL,
 	`PARTY_ID`	INT	NOT NULL,
-	`ALBUM_ID`	INT	NOT NULL,
-	`ALB_COMMENT_WRITER`	VARCHAR(255) 	NOT NULL,
+	`ALB_ID`	INT	NOT NULL,
+	`USER_ID`	VARCHAR(255) 	NOT NULL,
 	`ALB_COMMENT_CONTENT`	VARCHAR(255) 	NOT NULL,
-	`ALB_COMMENT_REG_DATE`	DATETIME	NOT NULL,
-	`ALB_COMMENT_MOD_DATE`	DATETIME	NOT NULL,
+	`ALB_COMMENT_REG_DATE`	DATETIME DEFAULT NOW(),
+	`ALB_COMMENT_MOD_DATE`	DATETIME NULL,
 	`ALB_COMMENT_AVAILABLE`	BOOLEAN	default true,
-    PRIMARY KEY(`ALB_COMMENT_ID`,`PARTY_ID`,`ALBUM_ID`)
+    PRIMARY KEY(`ALB_COMMENT_ID`,`PARTY_ID`,`ALB_ID`)
 );
 
 
@@ -248,8 +249,6 @@ ALTER TABLE `PARTY` ADD CONSTRAINT `FK_CATEGORY_TO_PARTY_1` FOREIGN KEY (
 REFERENCES `CATEGORY` (
 	`CATEGORY_NAME`
 );
-
-
 
 
 
@@ -402,7 +401,7 @@ REFERENCES `ALBUM` (
 );
 
 ALTER TABLE `ALBUM_COMMENT` ADD CONSTRAINT `FK_ALBUM_TO_ALBUM_COMMENT_2` FOREIGN KEY (
-	`ALBUM_ID`
+	`ALB_ID`
 )
 REFERENCES `ALBUM` (
 	`ALB_ID`
@@ -446,7 +445,7 @@ VALUES
 ('가족/결혼','marriage'),
 ('자유주제','free');
 
-select * from category;
+select * from USER;
 
 INSERT INTO `REPORT_CATEGORY` (REP_CATEGORY_NAME,REP_CATEGORY_VALUE)
 VALUES 
@@ -457,17 +456,50 @@ VALUES
 INSERT INTO USER (USER_ID,NAME,PASSWORD,BIRTHDAY,GENDER,LOCATION,
 PHONE_NUM,JOIN_DATE,ADMIN,PROFILE_IMAGE,MODIFIED_DAY,USER_AVAILABLE,
 MBTI_PATH,MBTI_ID)
-VALUES('ADMIN0','관리자','admin123',now(),'male','Korea',01012345678,now(),true,
-null,null,true,null,11);
+VALUES
+('ADMIN16','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,3),
+('ADMIN1','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,2),
+('ADMIN2','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,3),
+('ADMIN3','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,4),
+('ADMIN4','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,5),
+('ADMIN5','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,5),
+('ADMIN6','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,6),
+('ADMIN7','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,7),
+('ADMIN8','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,8),
+('ADMIN9','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,9),
+('ADMIN10','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,10),
+('ADMIN11','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,11),
+('ADMIN12','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,12),
+('ADMIN13','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,13),
+('ADMIN14','관리자','admin123',now(),'female','Korea',01012345678,now(),true,null,null,true,null,14),
+('ADMIN15','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,14);
+INSERT INTO USER (USER_ID,NAME,PASSWORD,BIRTHDAY,GENDER,LOCATION,
+PHONE_NUM,JOIN_DATE,ADMIN,PROFILE_IMAGE,MODIFIED_DAY,USER_AVAILABLE,
+MBTI_PATH,MBTI_ID)
+VALUES
+('로그인미구현대체','관리자','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,1);
 
-select * from USER join MBTI on user.MBTI_ID=mbti.MBTI_ID;
 
-
+INSERT INTO USER (USER_ID,NAME,PASSWORD,BIRTHDAY,GENDER,LOCATION,
+PHONE_NUM,JOIN_DATE,ADMIN,PROFILE_IMAGE,MODIFIED_DAY,USER_AVAILABLE,
+MBTI_PATH,MBTI_ID)
+VALUES
+('로그인미구현','로그인용','admin123',now(),'male','Korea',01012345678,now(),true,null,null,true,null,1);
 
 INSERT INTO `PARTY` (PARTY_title,PARTY_creator,PARTY_PRE_MBTI1,
 CATEGORY_NAME,PARTY_INTRODUCE,
 PARTY_LOCATION,PARTY_MAXIMUM,PARTY_REG_DATE)
- VALUES('테스트용 그룹','관리자','ENTP','업종/직무','열심히 코딩하는 곳입니다','한국소프트웨어인재개발원',30,now());
+ VALUES('테스트용 그룹','관리자','ENTP','업종/직무','열심히 코딩하는 곳입니다','한국소프트웨어인재개발원',30,now()),
+ ('테스트용 그룹2','관리자','ENTP','업종/직무','열심히 코딩하는 곳입니다','한국소프트웨어인재개발원',30,now());
+ 
+SELECT * FROM PARTY;
+
+
+
+
+
+
+
 
 
 
@@ -481,3 +513,47 @@ b.ART_REG_DATE from board b join `PARTY` g on b.PARTY_id=g.PARTY_id;
 INSERT INTO board_comment (PARTY_ID,ART_ID,ART_COMMENT_WRITER,ART_COMMENT_CONTENT,
 ART_COMMENT_REG_DATE) 
 VALUES (1,1,'관리자','테스트용 댓글입니다',now());
+
+
+
+SELECT A.ALB_ID, A.PARTY_ID, L.USER_ID, A.USER_ID, ALB_IMG_PATH,ALB_REG_DATE,ALB_AVAILABLE, COUNT(L.USER_ID) as likeCnt FROM ALBUM a left outer join like_list l
+	on a.alb_id=l.ALBUM_ID
+	WHERE ALB_AVAILABLE=TRUE AND a.PARTY_ID="1"
+    group by ALBUM_ID
+	ORDER BY ALB_ID DESC
+	LIMIT 10 OFFSET 0;
+    
+    SELECT * FROM ALBUM 
+	WHERE ALB_AVAILABLE=TRUE AND PARTY_ID="1" 
+	ORDER BY ALB_ID DESC
+	LIMIT 10 OFFSET 0;
+
+insert into member_list values ('admin2',2,1,now()),
+('admin3',2,1,now()),
+('admin4',2,1,now()),
+('admin5',2,1,now()),
+('admin6',2,1,now()),
+('admin7',2,1,now()),
+('admin8',2,1,now()),
+('admin9',2,1,now()),
+('admin10',2,1,now()),
+('admin11',2,1,now()),
+('admin12',2,1,now()),
+('admin13',2,1,now()),
+('admin14',2,1,now());
+
+insert into member_list (USER_ID,PARTY_ID,PARTY_REQUEST) values 
+('admin15',1,true),
+('admin2',1,true),
+('admin3',1,true),
+('admin4',1,true),
+('admin5',1,true),
+('admin6',1,true),
+('admin7',1,true),
+('admin8',1,true),
+('admin9',1,true),
+('admin10',1,true),
+('admin11',1,true),
+('admin12',1,true),
+('admin13',1,true),
+('admin14',1,true);
